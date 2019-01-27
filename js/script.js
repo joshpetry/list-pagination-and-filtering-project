47bb3639197690fa620dee5c163c8418b03fb41d
Josh Petry <40846033+joshpetry@users.noreply.github.com>
2019-01-27 15:41:14 -0500

    Finished showPage function

--- js/script.js
+++ js/script.js
@@ -3,62 +3,49 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
 const students = Array.prototype.slice.call(document.querySelectorAll('.student-item.cf'));
 const numberOfPages = Math.ceil(students.length / 10);
 
+// students 0 - 9 = iboya vat - connor taylor
+// students 10 - 19 = aymeric morel - florent gerard
+//students 20 - 29 = amber chen - emily harrison
 
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
-// figured out how to show record range - next step is to show page instead of range
-
-function showPage(low, high) {
-  const visiblePages = students.slice(low, high);
-
+// Shows student records for page selected
+function showPage(pageNumber) {
+  // Default visibleRecords slice range
+  let lowRange = 0;
+  let highRange = 10;
+  // visibleRecords slice range if page number is not 1
+  if (pageNumber !== 1) {
+    lowRange = (pageNumber * 10) - 10;
+    highRange = highRange + (pageNumber * 10) - 10;
+  };
+  // Range of student records for selected page
+  const visibleRecords = students.slice(lowRange, highRange);
+  // Hide all student records
   for ( let i = 0; i < students.length; i++ ) {
     students[i].style.display = 'none';
   }
-
-  for ( let i = 0; i < visiblePages.length; i++ ) {
-    visiblePages[i].style.display = 'block';
+  // Show student records for selected page
+  for ( let i = 0; i < visibleRecords.length; i++ ) {
+    visibleRecords[i].style.display = 'block';
   }
 }
 
-showPage(0, 5);
+showPage(1);
 
 /***
    Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
 
+/***
+// Pagination example code
 
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+<div class="pagination">
+  <ul>
+    <li><a href="">1</a></li>
+    <li><a href="">2</a></li>
+  </ul>
+</div>
+*/
